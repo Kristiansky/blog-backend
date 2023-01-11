176a304cd8c6ae72d603ccf168e5382434afbe27
Christian Dragnev <kristian9107@gmail.com>
2023-01-11 02:01:39 +0200

    Setting up project

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', Rules\Password::defaults()],
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+               'validation_errors' => $validator->messages()
+            ]);
+        }else{
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            $token = $user->createToken($user->email.'_token')->plainTextToken;
+
+            return response()->json([
+                'status' => 200,
+                'username' => $request->name,
+                'token' => $token,
+                'message' => 'Registered successfully.',
+            ]);
+        }
+    }
+
+    public function login(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'string', 'max:255'],
+        ]);
+
+        if($validator->fails()){
+            return response()->json([
+                'validation_errors' => $validator->messages()
+            ]);
+        }else{
+            $user = User::where('email', $request->email)->first();
+            if(!$user || !Hash::check($request->password, $user->password)){
+                return response()->json([
+                    'status' => 401,
+                    'message' => 'Invalid Credentials'
+                ]);
+            }else{
+                $token = $user->createToken($user->email.'_token')->plainTextToken;
+
+                return response()->json([
+                    'status' => 200,
+                    'username' => $user->name,
+                    'token' => $token,
+                    'message' => 'Logged in successfully.',
+                ]);
+            }
+        }
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Logged out successfully.'
+        ]);
+    }
+}
---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Post::select('id','title','description')->get();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title'=>'required',
+            'description'=>'required',
+            'user_id'=>'required|integer'
+        ]);
+
+        try{
+            Post::create($request->post());
+
+            return response()->json([
+                'message'=>'Post Created Successfully.'
+            ]);
+        }catch(\Exception $e){
+            return response()->json([
+                'message'=>'Something goes wrong while creating a post. \n' . $e->getMessage()
+            ],500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Post $post)
+    {
+        return response()->json([
+            'post'=>$post->load('comments'),
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        //
+    }
+
+    /**
+     * Display a count of all posts.
+     *
+     * @return int
+     */
+    public function postsCount(){
+        return Post::all()->count();
+    }
+}
